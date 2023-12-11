bab4beccf42ecace796a59c66655add2aea5eff1
Michelle Türtscher <mtuertscher.mmt-b2022@fh-salzburg.ac.at>
2023-12-11 17:13:12 +0100

    feat: Add Footer with Logo, Navigation and Contact

---
+++ src/components/common/ContactInfo.tsx
@@ -0,0 +1,12 @@
+const ContactInfo = () => {
+  return (
+    <div>
+      <p className="font-bold">Contact Information:</p>
+      <p>Name: John Doe</p>
+      <p>Address: 123 Main St, City</p>
+      <p>Phone: (123) 456-7890</p>
+      <p>Email: john.doe@example.com</p>
+    </div>
+  );
+};
+export default ContactInfo;
--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,7 +1,20 @@
+import ContactInfo from "../common/ContactInfo";
+import Logo from "../common/Logo";
+import Navigation from "../common/Navigation";
+
 const Footer = () => {
   return (
-    <footer>
-      <h1>My Footer</h1>
+    <footer className="p-10 container mx-auto flex justify-between items-center">
+      <div className="w-1/3 flex justify-center">
+        <Logo />
+      </div>
+      <div className="w-1/3 flex justify-center">
+        <Navigation />
+      </div>
+
+      <div className="w-1/3 flex justify-center">
+        <ContactInfo />
+      </div>
     </footer>
   );
 };
